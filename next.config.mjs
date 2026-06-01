ee8c1271d9588662f31b97a555ee8641020f1d49
javier aguilar <djspjam@outlook.com>
2026-05-31 23:35:38 -0600

    Actualización: Ajustes de seguridad aplicados

--- next.config.mjs
+++ next.config.mjs
@@ -29,6 +29,10 @@ const nextConfig = {
             key: 'X-XSS-Protection',
             value: '1; mode=block',
           },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; frame-src 'self' https://www.google.com; img-src 'self' data: blob:; connect-src 'self';",
+          },
         ],
       },
     ];
